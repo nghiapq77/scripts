7478cb846b79a08b805ce853d3b2a2bfff61522c
nghia.pq77 <nghia.pq77@gmail.com>
2020-04-22 23:33:43 +0700

    Add dwm scripts

--- tmux/current-session.sh
+++ tmux/current-session.sh
@@ -2,25 +2,21 @@
 
 if $(tmux has-session 2>/dev/null); then tmux -2u att; exit; fi
 tmux new-session -d -n ranger -s main -c /home/oz/
-sleep 0.2
+sleep 0.1
 tmux send-keys -t main:1.1 "ranger ." Enter
 tmux select-layout -t main:1 "ac3d,151x40,0,0,0" > /dev/null
 tmux new-window -n fs -t main:2 -c /home/oz/work/face-spoof
-sleep 0.2
+sleep 0.1
 tmux send-keys -t main:2.1 "source venv/bin/activate" Enter
 tmux select-layout -t main:2 "ae7a,151x40,0,0{93x40,0,0[93x15,0,0,1,93x24,0,16,5],57x40,94,0,2}" > /dev/null
 tmux split-window -t main:2 -c /home/oz/work/face-spoof/
-sleep 0.2
+sleep 0.1
 tmux send-keys -t main:2.2 "source venv/bin/activate" Enter
 tmux select-layout -t main:2 "ae7a,151x40,0,0{93x40,0,0[93x15,0,0,1,93x24,0,16,5],57x40,94,0,2}" > /dev/null
 tmux split-window -t main:2 -c /home/oz/work/face-spoof/data/
-sleep 0.2
+sleep 0.1
 tmux select-layout -t main:2 "ae7a,151x40,0,0{93x40,0,0[93x15,0,0,1,93x24,0,16,5],57x40,94,0,2}" > /dev/null
-tmux new-window -n obj-mat -t main:3 -c /home/oz/projects/object-matching
-sleep 0.2
-tmux send-keys -t main:3.1 "source venv/bin/activate" Enter
-tmux select-layout -t main:3 "ac43,151x40,0,0,6" > /dev/null
-tmux new-window -n bash -t main:4 -c /home/oz/
-sleep 0.2
-tmux select-layout -t main:4 "5658,151x40,0,0,19" > /dev/null
+tmux new-window -t main:3 -c /home/oz/
+sleep 0.1
+tmux select-layout -t main:3 "5658,151x40,0,0,19" > /dev/null
 tmux -2u att
